26597910780a23d609edaf9f757f1cefbe7c57e2
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-11-03 11:21:33 +0900

    マイグレーションファイルの編集

--- db/migrate/20211103021658_create_users.rb
+++ db/migrate/20211103021658_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
+      t.string :nickname, null: false
+      t.string :email, null: false
+      t.string :password, null: false
+      t.string :phone_number, null: false
       t.timestamps
     end
   end
