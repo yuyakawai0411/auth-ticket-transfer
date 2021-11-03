65deb2cc88ff703c53da7d8858ca40db828aaa3e
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-11-03 13:19:53 +0900

    モデルのアソシエーションとバリデーション設定

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,10 @@
 class Ticket < ApplicationRecord
+  belongs_to :user
+  
+  with_options presence: true do
+    validates :ticket_name
+    validates :event_date
+    validates :category_id
+    validates :status_id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :tickets
+  
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password
+    validates :phone_number
+  end
+
 end
