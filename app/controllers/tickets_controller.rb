3dc15e4c9550053d4e2bce0eb9fb6513cb6c3e26
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-11-03 14:11:38 +0900

    ユーザー照会機能

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,19 @@
 class TicketsController < ApplicationController
+  before_action :set_user_info, only: [:index, :show]
+
+  def index
+    @tickets = @user.tickets
+  end
+
+  def show
+    @ticket = @user.tickets.find_by(id: params[:id])
+  end
+
+
+  private
+
+  def set_user_info
+    @user = User.find(params[:id]).includes(:tickets)
+  end
+
 end
