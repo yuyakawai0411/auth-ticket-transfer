696ab346bbbaeb8ca61c40eaca2c06db782e01b0
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-11-03 18:37:18 +0900

    テストケースの雛形を作成

--- spec/requests/tickets_request_spec.rb
+++ spec/requests/tickets_request_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe "Tickets", type: :request do
-binding.pry
+
+  describe 'GET #index' do
+    context '存在するユーザーを検索した時' do
+      it 'ユーザーが所持するチケットが全て表示され、HTTP200が返される' do
+
+      end
+    end
+
+    context '存在しないユーザーを検索した時' do
+      it 'エラーメッセージが表示され、HTTP404が返される' do
+
+      end
+    end
+
+    context 'ユーザーを指定せずにリクエストした時' do
+      it 'エラーメッセージが表示され、HTTP404が返される' do
+
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    context '存在するユーザーを検索した時' do
+      context 'そのユーザーが所持しているチケットを検索した時' do
+        it 'チケットが表示され、HTTP200が返される' do
+
+        end
+      end
+      context 'そのユーザーが所持していないチケットを検索した時' do
+        it 'エラーメッセージが表示され、HTTP404が返される' do
+
+        end
+      end
+      context 'チケットを指定せず、リクエストした時' do
+        it 'エラーメッセージが表示され、HTTP404が返される' do
+
+        end
+      end
+    end
+
+    context '存在しないユーザーを検索した時' do
+      it 'エラーメッセージが表示され、HTTP404が返される' do
+
+      end
+    end
+
+    context 'ユーザーを指定せずにリクエストした時' do
+      it 'エラーメッセージが表示され、HTTP404が返される' do
+
+      end
+    end
+  end
+
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  describe 'GET #index' do
+    context 'ユーザー情報が存在する時' do
+      it 'ユーザー情報が表示され、HTTP200が返される' do
+
+      end
+    end
+
+    context 'ユーザー情報が存在しない時' do
+      it 'ユーザー情報が表示されず、HTTP200が返される' do
+
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    context '存在するユーザーを検索した時' do
+      it 'ユーザー情報が表示され、HTTP200が返される' do
+
+      end
+    end
+
+    context '存在しないユーザーを検索した時' do
+      it 'エラーメッセージが表示され、HTTP404が返される' do
+
+      end
+    end
+
+    context 'ユーザーを指定せずにリクエストした時' do
+      it 'エラーメッセージが表示され、HTTP404が返される' do
+
+      end
+    end
+  end
 
 end
