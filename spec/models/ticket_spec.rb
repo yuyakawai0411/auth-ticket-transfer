5d352b8cc900c65847a1c2f0b7ccd2eb37a872b7
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-11-05 11:48:34 +0900

    ユーザー照会機能とチケット照会機能のテストを記述

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'チケット取得' do
+  let(:ticket) { FactoryBot.build(:ticket) } 
+    context 'チケット取得できるとき' do
+      it '必須事項が全て存在すれば取得できる' do
+      expect(ticket).to be_valid
+      end
+    end
+
+    context 'チケット取得できないとき' do
+    subject { ticket.errors.full_messages }
+      it 'ticket_nameが空では取得できない' do
+        ticket.ticket_name = ''
+        ticket.valid?
+        is_expected.to include 
+      end
+      it 'event_dateが空では取得できない' do
+        ticket.event_date = ''
+        ticket.valid?
+        is_expected.to include 
+      end
+      it 'category_idが空では取得できない' do
+        ticket.category_id = ''
+        ticket.valid?
+        is_expected.to include 
+      end
+      it 'category_idが0以下では取得できない' do
+        ticket.category_id = ''
+        ticket.valid?
+        is_expected.to include 
+      end
+      it 'category_idが6以上では取得できない' do
+        ticket.category_id = ''
+        ticket.valid?
+        is_expected.to include 
+      end
+      it 'status_idが空では取得できない' do
+        ticket.status_id = ''
+        ticket.valid?
+        is_expected.to include 
+      end
+      it 'status_idが0以下では取得できない' do
+        ticket.status_id = ''
+        ticket.valid?
+        is_expected.to include 
+      end
+      it 'status_idが4以上では取得できない' do
+        ticket.status_id = ''
+        ticket.valid?
+        is_expected.to include 
+      end
+      it 'user_idが空では取得できない' do
+        ticket.user_id = ''
+        ticket.valid?
+        is_expected.to include 
+      end
+    end
+  end
 end
--- spec/requests/tickets_request_spec.rb
+++ spec/requests/tickets_request_spec.rb
@@ -4,19 +4,27 @@ RSpec.describe "Tickets", type: :request do
 
   describe 'GET #index' do
   let!(:user) { FactoryBot.create(:user) }
-  let(:user_not_exist) { FactoryBot.build(:user) }
+  let(:user_not_exist) { FactoryBot.build(:user)}
   let!(:ticket) { FactoryBot.create(:ticket, user_id: user.id) }
   let!(:ticket_other) { FactoryBot.create(:ticket, user_id: user.id) }
     context '存在するユーザーを検索した時' do
-      it 'ユーザーが所持するチケットが全て表示される' do
+      it 'ticket,ticket_otherが返される' do
         get "/users/#{user.id}/tickets"
         json = JSON.parse(response.body)
         expect(json['data'].length).to eq(2)
+      end
+      it 'ticketに正しい値がある' do
+        get "/users/#{user.id}/tickets"
+        json = JSON.parse(response.body)
         expect(json['data'][0]['id']).to eq(ticket.id)
         expect(json['data'][0]['ticket_name']).to eq(ticket.ticket_name)
         # expect(json['data'][0]['event_date']).to eq(ticket.event_date)
         expect(json['data'][0]['category']).to eq(ticket.category.name)
         expect(json['data'][0]['status']).to eq(ticket.status.name)
+      end
+      it 'ticket_otherに正しい値がある' do
+        get "/users/#{user.id}/tickets"
+        json = JSON.parse(response.body)
         expect(json['data'][1]['id']).to eq(ticket_other.id)
         expect(json['data'][1]['ticket_name']).to eq(ticket_other.ticket_name)
         # expect(json['data'][1]['event_date']).to eq(ticket_other.event_date)
@@ -31,10 +39,15 @@ RSpec.describe "Tickets", type: :request do
     end
 
     context '存在しないユーザーを検索した時' do
-      it 'エラーメッセージが表示され、HTTP404が返される' do
+      it 'エラーメッセージが返される' do
         # get "/users/#{user_not_exist.id}/tickets"
         # json = JSON.parse(response.body)
         # expect(json['data'].length).to eq(0) 
+        # expect(json['status']).to eq(200) 
+      end
+      it 'HTTP404が返される' do
+        # get "/users/#{user_not_exist.id}/tickets"
+        # json = JSON.parse(response.body)
         # expect(json['status']).to eq(200) 
       end
     end
@@ -44,40 +57,40 @@ RSpec.describe "Tickets", type: :request do
   let!(:user) { FactoryBot.create(:user) }
   let!(:ticket) { FactoryBot.create(:ticket, user_id: user.id) }
   let!(:ticket_other) { FactoryBot.create(:ticket, user_id: user.id) }
-    context '存在するユーザーを検索した時' do
-      context 'そのユーザーが所持しているチケットを検索した時' do
-        it 'チケットが表示され、HTTP200が返される' do
-          get "/users/#{user.id}/tickets/#{ticket.id}"
-          json = JSON.parse(response.body)
-          expect(json['data'].length).to eq(1)
-          expect(json['data'][0]['id']).to eq(ticket.id)
-          expect(json['data'][0]['ticket_name']).to eq(ticket.ticket_name)
-          # expect(json['data'][0]['event_date']).to eq(ticket.event_date)
-          expect(json['data'][0]['category']).to eq(ticket.category.name)
-          expect(json['data'][0]['status']).to eq(ticket.status.name)
-          expect(json['status']).to eq(200)
-        end
+  let(:ticket_not_exist) { FactoryBot.build(:ticket)}
+    context '存在するチケットを検索した時' do
+      it 'ticketデータが返される' do
+        get "/users/#{user.id}/tickets/#{ticket.id}"
+        json = JSON.parse(response.body)
+        expect(json['data'].length).to eq(1)
       end
-      context 'そのユーザーが所持していないチケットを検索した時' do
-        it 'エラーメッセージが表示され、HTTP404が返される' do
-          # get "/users/#{user_not_exist.id}/tickets"
-          # json = JSON.parse(response.body)
-          # expect(json['data'].length).to eq(0) 
-          # expect(json['status']).to eq(200) 
-        end
+      it 'ticketに正しい値がある' do
+        get "/users/#{user.id}/tickets/#{ticket.id}"
+        json = JSON.parse(response.body)
+        expect(json['data'][0]['id']).to eq(ticket.id)
+        expect(json['data'][0]['ticket_name']).to eq(ticket.ticket_name)
+        # expect(json['data'][0]['event_date']).to eq(ticket.event_date)
+        expect(json['data'][0]['category']).to eq(ticket.category.name)
+        expect(json['data'][0]['status']).to eq(ticket.status.name)
       end
-
+      it 'HTTP200が返される' do
+        get "/users/#{user.id}/tickets/#{ticket.id}"
+        json = JSON.parse(response.body)
+        expect(json['status']).to eq(200)
+      end
     end
 
-    context '存在しないユーザーを検索した時' do
-      it 'エラーメッセージが表示され、HTTP404が返される' do
-        # get "/users/#{user_not_exist.id}/tickets"
+    context '存在しないチケットを検索した時' do
+      it 'エラーメッセージが返される' do 
+        # get "/users/#{user.id}/tickets/#{ticket_not_exist}"
         # json = JSON.parse(response.body)
         # expect(json['data'].length).to eq(0) 
+      end
+      it 'HTTP404が返される' do
+        # get "/users/#{user.id}/tickets/#{ticket_not_exist}"
+        # json = JSON.parse(response.body)
         # expect(json['status']).to eq(200) 
       end
     end
-
   end
-
 end
