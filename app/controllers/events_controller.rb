78d0574bb9635ae577b21f31e7b99a97271e75bc
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-11-11 10:43:06 +0900

    eventsコントローラのアクション定義

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,26 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+    if @events.blank? 
+      render json: { status: 404, message: 'イベント登録はありません' }
+    else
+      @data = []
+      @events.each do |event|
+        @data << event.transfer_to_json
+      end
+      render json: { status: 200, data: @data }
+    end
+  end
+
+  def show
+    @event = Event.find_by(id: params[:id])
+    if @event.blank? 
+      render json: { status: 404, message: '存在しないイベントです' }
+    else
+      @data = @event.transfer_to_json
+      render json: { status: 200, data: @data }
+    end
+  end
+
 end
