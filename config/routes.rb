43eba7b64d7b4770cdb537d076147d37514e80d1
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-11-11 10:53:40 +0900

    eventsコントローラのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :tickets, only: [:index, :show, :create, :destroy] do
+    resources :tickets, only: [:index, :show] do
       resources :transitions, only: [:index, :show, :create]
     end
+  end
+  resources :events, only: [:index, :show] do
+    resources :tickets, only: [:create]
   end
 end
