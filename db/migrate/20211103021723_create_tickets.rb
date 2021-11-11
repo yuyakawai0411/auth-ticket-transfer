0e0f7ead96bcf3d046710b8b50b2a156b56065d5
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-11-12 08:27:17 +0900

    eventテーブルの変更

--- db/migrate/20211103021723_create_tickets.rb
+++ db/migrate/20211103021723_create_tickets.rb
@@ -1,9 +1,6 @@
 class CreateTickets < ActiveRecord::Migration[6.0]
   def change
     create_table :tickets do |t|
-      t.string :ticket_name, null: false
-      t.date :event_date, null: false
-      t.integer :category_id, null: false
       t.integer :status_id, null: false
       t.references :user, null: false, foreign_key: true
       t.references :event, null: false, foreign_key: true
